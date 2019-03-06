66a8e01f9715f9caa6a986050e03ab05625c5b3a
Robert McCary <robert.mccary@yahoo.com>
2019-03-06 15:38:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,21 +7,21 @@ class ApplicationController < Sinatra::Base
   get '/' do
     redirect '/recipes/new'
   end
-  
+
   get '/recipes/new' do
     erb :new
   end
-  
+
   post '/recipes' do
     @recipe=Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     redirect '/recipes/#{@recipe.id}'
   end
-  
+
   get '/recipes' do
     @recipes=Recipe.all
     erb :index
   end
-  
+
   get '/recipes/:id' do
     @recipe=Recipe.find_by_id(params[:id])
     erb :show
