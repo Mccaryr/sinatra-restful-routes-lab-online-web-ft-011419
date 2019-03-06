0e0a46a8397dd412086336f67274c7eaa0636476
Robert McCary <robert.mccary@yahoo.com>
2019-03-06 16:19:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,26 +7,26 @@ class ApplicationController < Sinatra::Base
   # get '/' do
   #   redirect '/recipes/new'
   # end
-  # 
+  #
   # get '/recipes/new' do
   #   erb :new
   # end
-  # 
+  #
   # get '/recipes' do
   #   @recipes=Recipe.all
   #   erb :index
   # end
-  # 
+  #
   # get '/recipes/:id' do
   #   @recipe=Recipe.find_by_id(params[:id])
   #   erb :show
   # end
-  # 
+  #
   # get '/recipes/:id/edit' do
   #   @recipe.Recipe.find_by_id(params[:id])
   #   erb :edit
   # end
-  # 
+  #
   # patch '/recipes/:id' do
   #   @recipe=Recipe.find_by_id(params[:id])
   #   @recipe.name=params[:name]
@@ -35,12 +35,12 @@ class ApplicationController < Sinatra::Base
   #   @recipe.save
   #   redirect '/recipes/#{@recipe.id}'
   # end
-  # 
+  #
   # post '/recipes' do
   #   @recipe=Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
   #   redirect '/recipes/#{@recipe.id}'
   # end
-  # 
+  #
   # delete '/recipes/:id' do
   #   @recipe=Recipe.find_by_id(params[:id])
   #   @recipe.delete
