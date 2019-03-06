d37ec318fb07660423ec16fb920bf0e42c932b30
Robert McCary <robert.mccary@yahoo.com>
2019-03-06 16:40:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,10 +37,10 @@ class ApplicationController < Sinatra::Base
     redirect '/recipes/#{@recipe.id}'
   end
 
-  post '/recipes' do  #creates a recipe
-     @recipe = Recipe.create(params)
-     redirect to "/recipes/#{@recipe.id}"
-   end
+  post '/recipes' do
+    @recipe=Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect '/recipes/#{@recipe.id}'
+  end
 
   delete '/recipes/:id' do
     @recipe=Recipe.find_by_id(params[:id])
